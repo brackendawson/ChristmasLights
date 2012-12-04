02337adff60ff795c2f03ffa93d18303561a200b
Bracken Dawson <abdawson@gmail.com>
2012-12-04 22:38:58 +0000

    Move to 16-bit High Color
    And tune the colors too.

--- main.c
+++ main.c
@@ -3,15 +3,16 @@
 
 #include <msp430.h>
 
-#define	RED	0xFF,0x00,0x00
-#define ORANGE	0xFF,0x20,0x00
-#define YELLOW	0xFF,0x70,0x00
-#define	GREEN	0x00,0xFF,0x00
-#define BLUE	0x00,0x00,0xFF
-#define INDIGO	0xFF,0x00,0x50
-#define WHITE   0xFF,0xFF,0xFF
+#define	RED	(31<<11)+(0*2<<5)+0
+#define ORANGE	(31<<11)+(3*2<<5)+0
+#define YELLOW	(31<<11)+(12*2<<5)+0
+#define	GREEN	(0<<11)+(31*2<<5)+0
+#define BLUE	(0<<11)+(0*2<<5)+31
+#define INDIGO	(12<<11)+(0*2<<5)+8
+#define WHITE	(31<<11)+(31*2<<5)+31
+//               ^r       ^g       ^b  16-bit High Color
 
-void send(unsigned char r, unsigned char g, unsigned char b);
+void send(unsigned int c);
 
 int main(void) {
 
@@ -51,22 +52,22 @@ _BIS_SR(LPM4_bits);	//Go into Low power mode 4, main stops here
 
 }
 
-void send(unsigned char r, unsigned char g, unsigned char b) {
+void send(unsigned int c) {
   //R
   //wait for USIIFG to be clear
   while (0 == (USICTL1 & USIIFG)) {}
   USICTL1 = USICTL1 - USIIFG;
-  USISRL = r; //load the 8 "red" bits into the buffer
+  USISRL = c >> 8 & 0xF8; //load the 8 "red" bits into the buffer
   USICNT = 8;       //tell the USI to clock 8 bits out
   //G
   while (0 == (USICTL1 & USIIFG)) {}
   USICTL1 = USICTL1 - USIIFG;
-  USISRL = g;
+  USISRL = c >> 3 & 0xFC;
   USICNT = 8;
   //B
   while (0 == (USICTL1 & USIIFG)) {}
   USICTL1 = USICTL1 - USIIFG;
-  USISRL = b;
+  USISRL = c << 3;
   USICNT = 8;
   return;
 }
