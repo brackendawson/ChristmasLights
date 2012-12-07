04560fdc2f6a8909c6d56675a622b61bd015e952
Bracken Dawson <abdawson@gmail.com>
2012-12-07 22:20:27 +0000

    Totally interrupt driven animation MILESTONE
    Totally interrupt driven precession of lights. Next need to move
    it to a header and sort out multi modes.

--- main.c
+++ main.c
@@ -10,11 +10,12 @@
 #define INDIGO	0xFF,0x00,0x50
 #define WHITE   0xFF,0xFF,0xFF
 
-/* This flag is true while the USI is transmitting and is
-cleared by interrupt. It must be false before another
-transmission can begin. It must be volatile to stop GCC
-optimizing it away.*/
-_Bool volatile txing = 0;
+unsigned char buffer[] = {RED,ORANGE,YELLOW,GREEN,BLUE,INDIGO};
+unsigned char buffer_byte = 0;
+unsigned char buffer_loops_left = 0;
+#define BUFFER_LOOPS	17  // 100/6
+
+unsigned char rotate = 0;
 
 void send(unsigned char r, unsigned char g, unsigned char b);
 
@@ -31,7 +32,7 @@ BCSCTL1 = CALBC1_1MHZ;
 
 //setup USI
 //        edges     interrupt enabe (this row clear USIIFG)
-USICTL1 = USICKPH | USIIE;
+USICTL1 = USICKPH;// | USIIE;
 //         SMCLK      no div
 USICKCTL = USISSEL_2;
 //        Output and clk             output enable   master    this reanables the USI
@@ -45,16 +46,6 @@ WRITE_SR(GIE);
 // starts timer, going for 40ms 
 TACCR0 = 0x9C40;
 
-unsigned char i;
-for ( i = 0 ; i < 17 ; i++ ) {
-  send(RED);
-  send(ORANGE);
-  send(YELLOW);
-  send(GREEN);
-  send(BLUE);
-  send(INDIGO);
-}
-
 /* Go into Low power mode , main stops here.
 CPU, MCLK are disabled, SMCLK, ACLK  and DCO
 are active. */
@@ -62,33 +53,61 @@ _BIS_SR(LPM1_bits);
 
 }
 
-void send(unsigned char r, unsigned char g, unsigned char b) {
-  //R
-  while (txing) {}  //wait for USIIFG to be clear
-  USISRL = r;       //load the 8 "red" bits into the buffer
-  txing = 1;        //Set the flag to stop thenext transmission
-  USICNT = 8;       //tell the USI to clock 8 bits out
-  //G
-  while (txing) {}
-  USISRL = g;
-  txing = 1;
+/*sends the first byte down the string. Interrupt
+driven code handles the rest. */
+void startsend(void) {
+  //enable USI interrput
+  USICTL1 = USICTL1 + USIIE;
+  //handle counters
+  buffer_loops_left = BUFFER_LOOPS;
+  //load the first byte
+  USISRL = buffer[buffer_byte = 0];
   USICNT = 8;
-  //B
-  while (txing) {}
-  USISRL = b;
-  txing = 1;
-  USICNT = 8;
-  return;
 }
 
 /* This function should be called 25 times per second. */
 interrupt (TIMERA1_VECTOR) TimerA1ServerRoutine(void) {
   TACTL = TACTL - TAIFG; //reset Timer A interrupt flag
+  
+  rotate++;
+  //rotate the array by one every 25th pass
+  if (12 == rotate) {
+    rotate = 0;
+    unsigned char storr = buffer[0];
+    unsigned char storg = buffer[1];
+    unsigned char storb = buffer[2];
+    unsigned char i;
+    for (i = 0 ; i < sizeof(buffer) - 3 ; i+=3) {
+      buffer[i] = buffer[i+3];
+      buffer[i+1] = buffer[i+4];
+      buffer[i+2] = buffer[i+5];    
+    }
+    buffer[sizeof(buffer) - 3] = storr;
+    buffer[sizeof(buffer) - 2] = storg;
+    buffer[sizeof(buffer) - 1] = storb;
+  }
+
+  startsend();  //MUST BE AFTER ALL EDITS OF BUFFER!
 }
 
 /* This function should be called when the USI has
 finished tranmising the data in it's buffer. */
 interrupt (USI_VECTOR) USIServiceRoutine(void) {
   USICTL1 = USICTL1 - USIIFG;
-  txing = 0;
+  //handle counters
+  if (buffer_byte >= sizeof(buffer) - 1) {
+    buffer_byte = 0;
+    buffer_loops_left--;
+    //end condition
+    if (0 == buffer_loops_left) {
+      //disable interrupt
+      USICTL1 = USICTL1 - USIIE;
+      return;
+    }
+  } else {
+    buffer_byte++;
+  }
+  //load a byte
+  USISRL = buffer[buffer_byte];
+  USICNT = 8;
 }
